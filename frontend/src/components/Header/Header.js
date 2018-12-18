1727f715ec3d6e327791cc88b091a005b6775c2c
junseok5 <vkehrkrj@naver.com>
2018-12-18 14:17:50 +0900

    Header completed

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,14 +2,24 @@ import React from 'react'
 import cx from 'classnames'
 import './Header.scss'
 
+import { FiSearch } from 'react-icons/fi'
+
 const Header = () => (
   <header>
-    <div className={cx('header-bg')}>
-      <img src={require('static/images/bg_1.jpg')} draggable="false" />
-    </div>
     <div className={cx('header-main')}>
-      <div className={cx('hm-contents')}>
-        <div className={cx('hm-logo')}>심슨영어</div>
+      <div className={cx('hm-logo')}>심슨영어</div>
+      <div className={cx('hm-search')}>
+        <label className={cx('search-label')}>
+          {/* <div className={cx('search-icon _p-ps')}> */}
+          <FiSearch fontSize="1.3rem" color="#747474" />
+          {/* </div> */}
+          <div className={cx('search-form')}>
+            <input type="search" placeholder="검색" />
+          </div>
+        </label>
+      </div>
+      <div className={cx('hm-search-mobile')}>
+        <FiSearch fontSize="1.4rem" />
       </div>
     </div>
   </header>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,11 +3,7 @@ import PageTemplate from 'components/PageTemplate'
 import SearchBox from 'components/SearchBox'
 
 const Home = () => {
-  return (
-    <PageTemplate>
-      <SearchBox />
-    </PageTemplate>
-  )
+  return <PageTemplate>{/* <SearchBox /> */}</PageTemplate>
 }
 
 export default Home
