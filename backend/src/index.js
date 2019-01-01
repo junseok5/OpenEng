4e1ef7f2821e4b30f36c22052f109215b361b8bf
junseok5 <vkehrkrj@naver.com>
2019-01-02 01:18:45 +0900

    Build and server render

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,14 @@ require('dotenv').config()
 
 const Koa = require('koa')
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
+const path = require('path')
+const views = require('koa-views')
+const serve = require('koa-static')
 const db = require('./database')
 const api = require('./api')
+
+const staticPath = path.join(__dirname, '../../frontend/build')
 
 db.connect()
 
@@ -16,7 +22,17 @@ const router = new Router()
 // 라우터 설정
 router.use('/api', api.routes())
 
+app.use(bodyParser())
 app.use(router.routes()).use(router.allowedMethods())
+app.use(serve(staticPath))
+app.use(
+  views(staticPath, {
+    extension: 'html',
+  }),
+)
+app.use(async ctx => {
+  await ctx.render('index.html')
+})
 
 app.listen(port, () => {
   console.log('Listenint to port 4000')
