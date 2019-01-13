74365c627a5ba39914dbd3c8029c474883333729
junseok5 <vkehrkrj@naver.com>
2019-01-14 00:51:29 +0900

    prev codesplitting code delete

--- frontend/src/pages/index.async.js
+++
@@ -1,6 +0,0 @@
-import asyncComponent from 'lib/asyncComponent'
-
-export const Home = asyncComponent(() => import('./Home'))
-export const Search = asyncComponent(() => import('./Search'))
-export const Videos = asyncComponent(() => import('./Videos'))
-export const NotFound = asyncComponent(() => import('./NotFound'))
--- frontend/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home'
-export { default as Videos } from './Videos'
-export { default as Search } from './Search'
-export { default as NotFound } from './NotFound'
