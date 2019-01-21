da8230f3cc92c7abafda0662136b303ac9f6156c
junseok5 <vkehrkrj@naver.com>
2019-01-21 09:51:05 +0900

    letsencrypt test - 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
 
+const createServer = require('auto-sni')
+// const http = require('http')
+// const https = require('https')
+
 const Koa = require('koa')
 const Router = require('koa-router')
 const KoaBody = require('koa-body')
@@ -7,27 +11,17 @@ const path = require('path')
 const views = require('koa-views')
 const serve = require('koa-static')
 const authToken = require('lib/middlewares/authToken')
-const db = require('./database')
-const api = require('./api')
 
 const staticPath = path.join(__dirname, '../../frontend/build')
 
-db.connect()
+const { HTTP_PORT: httpPort, HTTPS_PORT: httpsPort } = process.env
 
-const { PORT: port } = process.env
 const app = new Koa()
 
 app.use(KoaBody({ multipart: true }))
 app.use(serve('public'))
 app.use(serve(staticPath))
 app.use(authToken)
-
-// 라우터 설정
-const router = new Router()
-
-router.use('/api', api.routes())
-app.use(router.routes()).use(router.allowedMethods())
-
 app.use(
   views(staticPath, {
     extension: 'html'
@@ -37,6 +31,33 @@ app.use(async ctx => {
   await ctx.render('index.html')
 })
 
-app.listen(port, () => {
-  console.log(`Listenint to port ${port}`)
-})
+const db = require('./database')
+
+db.connect()
+
+// 라우터 설정
+const api = require('./api')
+
+const router = new Router()
+
+router.use('/api', api.routes())
+app.use(router.routes()).use(router.allowedMethods())
+
+createServer(
+  {
+    email: 'vkehrkrl82@gmail.com',
+    agreeTos: true,
+    debug: true,
+    domains: ['openeng.org', ['openeng.org', 'www.openeng.org']],
+    dir: '~/etc/letsencrypt',
+    ports: {
+      http: httpPort,
+      https: httpsPort
+    }
+  },
+  app.callback()
+)
+
+// app.listen(httpPort, () => {
+//   console.log(`Listenint to port ${httpPort}`)
+// })
