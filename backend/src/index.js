761a4988a9e6ef11552260ed516432cf08a2ae43
junseok5 <vkehrkrj@naver.com>
2019-01-21 09:59:53 +0900

    letsencrypt debug false

--- backend/src/index.js
+++ backend/src/index.js
@@ -47,7 +47,7 @@ createServer(
   {
     email: 'vkehrkrl82@gmail.com',
     agreeTos: true,
-    debug: true,
+    debug: false,
     domains: ['openeng.org', ['openeng.org', 'www.openeng.org']],
     dir: '~/etc/letsencrypt',
     ports: {
