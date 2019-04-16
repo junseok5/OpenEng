988172322d63e871414f9467018daa56e768c092
junseok5 <vkehrkrj@naver.com>
2019-04-16 17:00:38 +0900

    project test

--- backend/src/api/v1.0/videos/videos.ctrl.js
+++ backend/src/api/v1.0/videos/videos.ctrl.js
@@ -1,49 +1,49 @@
-const Video = require('database/models/Video')
+const Video = require("database/models/Video")
 // const Joi = require('joi')
 
 exports.list = async ctx => {
-  const page = parseInt(ctx.query.page || 1, 10)
-  const { tag, channel, keyword } = ctx.query
+    const page = parseInt(ctx.query.page || 1, 10)
+    const { tag, channel, keyword } = ctx.query
 
-  const baseQuery = { published: 'public' }
-  let query = {}
-  query = channel && { ...baseQuery, channel }
-  query = tag && { ...baseQuery, tags: tag }
-  query = keyword && {
-    ...baseQuery,
-    $text: { $search: keyword }
-  }
-  // : { ...query }
+    const baseQuery = { published: "public" }
+    let query = {}
+    query = channel && { ...baseQuery, channel }
+    query = tag && { ...baseQuery, tags: tag }
+    query = keyword && {
+        ...baseQuery,
+        title: { $regex: keyword, $options: "i" }
+    }
+    // : { ...query }
 
-  if (page < 1) {
-    ctx.status = 400
-    return
-  }
+    if (page < 1) {
+        ctx.status = 400
+        return
+    }
 
-  try {
-    const videos = await Video.findList(query, page)
+    try {
+        const videos = await Video.findList(query, page)
 
-    ctx.body = videos
-  } catch (e) {
-    ctx.throw(e, 500)
-  }
+        ctx.body = videos
+    } catch (e) {
+        ctx.throw(e, 500)
+    }
 }
 
 exports.read = async ctx => {
-  const { id } = ctx.params
+    const { id } = ctx.params
 
-  try {
-    const video = await Video.findById(id).exec()
+    try {
+        const video = await Video.findById(id).exec()
 
-    if (!video) {
-      ctx.status = 404
-      return
+        if (!video) {
+            ctx.status = 404
+            return
+        }
+        ctx.body = video
+
+        // 조회 수 증가
+        video.updateOne({ views: video.views + 1 })
+    } catch (e) {
+        ctx.throw(e, 500)
     }
-    ctx.body = video
-
-    // 조회 수 증가
-    video.updateOne({ views: video.views + 1 })
-  } catch (e) {
-    ctx.throw(e, 500)
-  }
 }
